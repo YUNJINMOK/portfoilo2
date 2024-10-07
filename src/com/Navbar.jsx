800dd2c9f61dc8dc7f9252bb8d0290964cf71570
Yunjinmok <aprnaprn114@naver.com>
2024-10-07 14:58:51 +0900

    re

--- src/com/Navbar.jsx
+++ src/com/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <div className="w-[13%] h-screen fixed left-0 bottom-0 flex flex-col  items-center shadow-md bg-[#ffff]">
-            <Link to="/" className="py-4 mt-[50px]">
+            <Link to="/" className="py-8 mt-[50px]">
                 <span className="font-semibold text-4xl">Hello!</span>
             </Link>
-            <div className="w-full h-[600px]  flex flex-col justify-center items-center gap-10 mt-10">
+            <div className="w-full h-[600px]  flex flex-col justify-center items-center gap-10 mt-">
                 <Link to="/">
                     <div className="font-semibold text-xl border-b border-gray-300 w-[120px] text-center mb-4 pb-4 cursor-pointer">
                         Home
@@ -21,11 +21,6 @@ export default function Navbar() {
                 <Link to="/">
                     <div className="font-semibold text-xl border-b border-gray-300 w-[120px] text-center mb-4 pb-4 cursor-pointer">
                         Portfoilo
-                    </div>
-                </Link>
-                <Link to="/">
-                    <div className="font-semibold text-xl border-b border-gray-300 w-[120px] text-center mb-4 pb-4 cursor-pointer">
-                        Contact
                     </div>
                 </Link>
             </div>
--- src/router/About.jsx
+++ src/router/About.jsx
@@ -27,17 +27,110 @@ export default function About() {
                 >
                     <div className="w-full h-screen bg-gray-200 overflow-hidden">
                         <div className="w-full h-full ml-[300px] flex justify-center p-8">
-                            <div className="max-w-4xl w-full h-full bg-white mr-[300px]  flex justify-center">
-                                <div className="w-1/2 h-full bg-gray-600 flex flex-col items-center py-16">
-                                    <div className="w-[200px] h-[200px] bg-gray-300 rounded-full flex justify-center items-center">
-                                        <div className="w-[190px] h-[190px] bg-white rounded-full"></div>
+                            <div className="max-w-4xl w-full h-full bg-white mr-[300px]  flex justify-center rounded-xl overflow-hidden">
+                                <div className="w-1/3 h-full bg-blue-100 flex flex-col items-center py-10 ">
+                                    <div className="w-[160px] h-[160px] bg-blue-500 rounded-full flex justify-center items-center z-10">
+                                        <div className="w-[150px] h-[150px] bg-white rounded-full z-10"></div>
                                     </div>
-                                    <div className="w-full h-[100px] flex flex-col justify-center items-center gap-2">
-                                        <span className="text-2xl font-semibold text-white">윤진목</span>
-                                        <span className="text-xl font-semibold text-white">YUN JIN MOK</span>
+                                    <div className="w-full h-2/3  flex justify-center mt-4  ">
+                                        <div className="max-w-[250px] w-full h-full overflow-hidden ">
+                                            <div className="w-full h-[60px] flex items-center">
+                                                <span className="text-xl font-semibold text-blue-600">개인 정보</span>
+                                                <div className="flex-grow border-t border-blue-600 ml-4"></div>
+                                            </div>
+                                            <div className="w-full h-[200px] ml-2 flex flex-col items-start gap-3">
+                                                <span className="text-sm font-semibold">윤진목</span>
+                                                <span className="text-sm font-semibold">1999년07월06일</span>
+                                                <span className="text-sm font-semibold">010-2423-2601</span>
+                                                <span className="text-sm font-semibold">aprnaprn114@naver.com</span>
+                                                <span className="text-sm font-semibold">
+                                                    경북 경산시 신대리 512
+                                                    <br /> 천년사랑 502호
+                                                </span>
+                                            </div>
+                                            <div className="w-full h-[60px] flex items-center">
+                                                <span className="text-xl font-semibold text-blue-600">자격증</span>
+                                                <div className="flex-grow border-t border-blue-600 ml-4"></div>
+                                            </div>
+                                            <div className="w-full h-[200px] ml-2 flex flex-col items-start gap-3">
+                                                <div className=" flex items-end gap-3">
+                                                    <span className="text-sm font-semibold">2017.07</span>
+                                                    <span className="text-lg font-semibold">GTQ포토샵2급</span>
+                                                </div>
+                                                <div className=" flex items-end gap-3">
+                                                    <span className="text-sm font-semibold">2018.10</span>
+                                                    <span className="text-lg font-semibold">1종보통운전면허</span>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="w-1/2 h-full "></div>
+                                <div className="w-2/3 h-full flex flex-col items-center py-5 ">
+                                    <div className="max-w-lg w-full h-full ">
+                                        <div className="w-full h-[200px] flex flex-col gap-4 justify-center ">
+                                            <span className="text-xl font-semibold ">
+                                                성실하고 <span className="text-blue-600">열정</span> 넘치는
+                                            </span>
+                                            <span className="text-4xl font-semibold">
+                                                <span className="text-blue-600">윤진목</span> 입니다
+                                            </span>
+                                        </div>
+                                        <div className="w-full h-full">
+                                            <div className="w-full h-[150px]">
+                                                <div className="w-full h-[60px] flex items-center">
+                                                    <span className="text-xl font-semibold text-blue-600">
+                                                        학력사항
+                                                    </span>
+                                                    <div className="flex-grow border-t border-blue-600 ml-4"></div>
+                                                </div>
+                                                <div className="w-full h-[200px] ml-2 flex flex-col items-start gap-3">
+                                                    <div className=" flex items-end gap-3">
+                                                        <span className="text-sm font-semibold">2015.03 ~ 2018.02</span>
+                                                        <span className="text-lg font-semibold">
+                                                            경산 고등학교(졸업)
+                                                        </span>
+                                                    </div>
+                                                    <div className=" flex items-end gap-3">
+                                                        <span className="text-sm font-semibold">2018.04 ~ 2019.02</span>
+                                                        <span className="text-lg font-semibold">
+                                                            계명문화대학교(건축인테리어과)자퇴
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="w-full h-[150px]">
+                                                <div className="w-full h-[60px] flex items-center">
+                                                    <span className="text-xl font-semibold text-blue-600">교육</span>
+                                                    <div className="flex-grow border-t border-blue-600 ml-4"></div>
+                                                </div>
+                                                <div className="w-full h-[200px] ml-2 flex flex-col items-start gap-3">
+                                                    <div className=" flex items-end gap-3">
+                                                        <span className="text-sm font-semibold">2023.11 ~ 2024.06</span>
+                                                        <span className="text-lg font-semibold">
+                                                            영진직업전문학교 [K-Digital Training] OpenAPI를 활용한
+                                                            스마트웹&앱(프론트엔드) 개발자 양성과정
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="w-full h-[150px]">
+                                                <div className="w-full h-[60px] flex items-center">
+                                                    <span className="text-xl font-semibold text-blue-600">수상</span>
+                                                    <div className="flex-grow border-t border-blue-600 ml-4"></div>
+                                                </div>
+                                                <div className="w-full h-[200px] ml-2 flex flex-col items-start gap-3">
+                                                    <div className=" flex items-end gap-3">
+                                                        <span className="text-sm font-semibold">2024.06.03</span>
+                                                        <span className="text-lg font-semibold">
+                                                            제14회 송암전 영진직업전문학교
+                                                            <br /> 대구수목원 스탬프 투어 하이브리드 앱(최우수상)
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
